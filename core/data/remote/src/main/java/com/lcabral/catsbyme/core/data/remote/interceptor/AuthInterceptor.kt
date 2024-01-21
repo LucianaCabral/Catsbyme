98327f36b36d9206bc2cbf152a807ca3b2a01691
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-01-20 21:18:47 -0300

    :bug: fix:[CBM-ANY] Fixed data AuthInterceptor network

--- core/data/remote/src/main/java/com/lcabral/catsbyme/core/data/remote/interceptor/AuthInterceptor.kt
+++ core/data/remote/src/main/java/com/lcabral/catsbyme/core/data/remote/interceptor/AuthInterceptor.kt
@@ -1,27 +1,24 @@
 package com.lcabral.catsbyme.core.data.remote.interceptor
 
+import com.lcabral.catsbyme.core.data.remote.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
 
 private const val API_KEY = "api_key"
 
-internal class AuthInterceptor(
-    private val apiKey: String
-) : Interceptor {
+internal class AuthInterceptor : Interceptor {
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        val requestUrl = request.url()
-        val newUrl = requestUrl.newBuilder()
-            .addQueryParameter(API_KEY, apiKey)
+        val newUrl = request.url.newBuilder()
+            .addQueryParameter(API_KEY, BuildConfig.API_KEY)
+            .build()
+        val newRequest =request.newBuilder()
+            .url(newUrl)
             .build()
 
-        return chain.proceed(
-            request.newBuilder()
-                .url(newUrl)
-                .build()
-        )
+        return chain.proceed(newRequest)
     }
 }
